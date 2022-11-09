3a933ff21262138fec20f52cdb04d2df9f2503d5
Guilherme <guilhermemarmeirelles@gmail.com>
2022-11-09 18:29:46 +0000

    Added routes, route guards. Changed theme

---
+++ client/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memberlist',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
